e5aafe8e1e2b9ea47192e7120318a71015f56ec4
Hamurabi Araújo <araujohamurabi@gmail.com>
2017-08-29 17:51:19 -0300

    Showing missing data

--- q1.py
+++ q1.py
@@ -1,35 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-Este é um arquivo de script temporário.
-"""
-
-
 from csv import reader
 from matplotlib import pyplot as plt
+from math import pow, sqrt
 
 with open('data.csv', 'rb') as csvFile:
     csvReader = reader(csvFile, delimiter=';', quotechar='|')
   
-    v = []
-    average = []
-  
-    for row in csvReader:
-        v.append(row)
-        average.append(( float(row[1]) + float(row[2])) / 2)
-        
-    print len(average)
+    averages = []
+    total = 0
+    summ = 0
     
-    plt.bar(range(1,31), average)
+    for row in csvReader:
+        averages.append(( float(row[1]) + float(row[2])) / 2)
+        total += float(row[1]) + float(row[2]) / 2
+        
+    av = total / len(averages)
+    
+    for a in averages :
+        summ += pow((a - av), 2)
+    
+    print 'Amplitude: ' + format(max(averages) - min(averages))
+    print 'Desvio padrão: ' + format(sqrt(summ / len(averages)))
+    
+    plt.bar(range(1,31), averages)
     plt.title("Grafico de medias")
     plt.ylabel("Nota")
     plt.xlabel("Aluno")
     plt.show()
-    
-    
-    
-      
-  
-      
-   
