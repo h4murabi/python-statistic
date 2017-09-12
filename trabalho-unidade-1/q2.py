c6ad2ac35375df2a53afdbbbfb6b27b03b1c2771
Paulo Sergio <paulosergios11@hotmail.com>
2017-09-12 16:32:50 -0300

    correção na analise de 2015

--- trabalho-unidade-1/q2.py
+++ trabalho-unidade-1/q2.py
@@ -19,6 +19,45 @@ def takeValue(el):
 def takeDiff(el):
     return el['diffDate']
 
+with open('2015.csv', 'r') as f_diarias_2015:
+    csvReader = csv.reader(f_diarias_2015, delimiter=',')
+
+    # Organizing the data
+    filteredData = []
+    index = 0
+
+    for row in csvReader:
+        if (row[4]).startswith('R$') :
+            filteredData.append({
+                'person': row[0],
+                'startDate': datetime.datetime.strptime(row[1], '%d/%m/%Y'),
+                'endDate': datetime.datetime.strptime(row[2], '%d/%m/%Y'),
+                'diffDate': -1,
+                'description': row[3],
+                'value': Decimal(sub(r'[^\d.]', '', string.replace(row[4], '.', ',')))
+            })
+            filteredData[index]['diffDate'] = (filteredData[index]['endDate'] - filteredData[index]['startDate']).days
+            index = index + 1
+
+    dataByValue = copy.copy(filteredData)
+    dataByValue.sort(key=takeValue)
+
+    dataByDiff = copy.copy(filteredData)
+    dataByDiff.sort(key=takeDiff)
+    
+    print "\nANO 2015"
+    print 'VARIAVEL: Valor das Diarias'
+    print('Valor Minimo: R$' + str(dataByValue[0]['value']) + ' ( ' + dataByValue[0]['person'] + ' ) ')
+    print('Valor Maximo: R$' + str(dataByValue[len(dataByValue) - 1]['value']) + ' ( ' + dataByValue[len(dataByValue) - 1]['person'] + ' ) ' )
+    print 'Amplitude: ', str((dataByValue[len(dataByValue) - 1]['value']) - (dataByValue[0]['value'] / 100))
+
+    print ''
+    
+    print 'VARIAVEL: Quantidade de dias das diarias'
+    print('Valor Minimo: ' + str(dataByDiff[0]['diffDate']) + ' dias ( ' + dataByDiff[0]['person'] + ' ) ')
+    print('Valor Maximo: ' + str(dataByDiff[len(dataByDiff) - 1]['diffDate']) + ' dias ( ' + dataByDiff[len(dataByDiff) - 1]['person'] + ' ) ' )
+    print 'Amplitude: ', str((dataByDiff[len(dataByDiff) - 1]['diffDate']) - (dataByDiff[0]['diffDate']))
+
 
 with open('2016.csv', 'r') as f_diarias_2016:
     csvReader = csv.reader(f_diarias_2016, delimiter=',')
@@ -46,6 +85,7 @@ with open('2016.csv', 'r') as f_diarias_2016:
     dataByDiff = copy.copy(filteredData)
     dataByDiff.sort(key=takeDiff)
     
+    print "\n\nANO 2016"
     print 'VARIAVEL: Valor das Diarias'
     print('Valor Minimo: R$' + str(dataByValue[0]['value'] / 100) + ' ( ' + dataByValue[0]['person'] + ' ) ')
     print('Valor Maximo: R$' + str(dataByValue[len(dataByValue) - 1]['value'] / 100) + ' ( ' + dataByValue[len(dataByValue) - 1]['person'] + ' ) ' )
